24592073978af6ff12cb8de78d6a3030b36b7a5a
bruno <bruno.santossilveira001@gmail.com>
2025-03-25 17:00:56 -0300

    Home page and initied the project structure. First steps in animations.

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,19 +1,76 @@
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
 import styles from './home.module.css'
 
 export default function Home() {
+	const [showText, setShowText] = useState('')
+	const [cursorState, setCursorState] = useState('|')
+	const navigate = useNavigate()
+	const fullText = 'Amanda Passos'
+
+	useEffect(() => {
+		let index = 0
+
+		// Efeito de digitação
+		const typingInterval = setInterval(() => {
+			setShowText(fullText.slice(0, index + 1))
+			index++
+
+			if (index >= fullText.length) {
+				clearInterval(typingInterval)
+			}
+		}, 200)
+
+		// Efeito de cursor piscando
+		const cursorInterval = setInterval(() => {
+			setCursorState((prev) => (prev === '|' ? '' : '|')); 
+		}, 500)
+
+		// Timer para transição para a próxima página
+		const timeoutId = setTimeout(() => {
+			navigate('/main')
+		}, 155000)
+
+		// Cleanup
+		return () => {
+			clearInterval(typingInterval)
+			clearInterval(cursorInterval)
+			clearTimeout(timeoutId)
+		}
+	}, [navigate])
 
 	return (
-		<div className={styles.homeContainer}>
-			<img src="" alt="" />
+		<motion.section
+			className={styles.homeContainer}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 1 }}
+		>
 			<div className={styles.title}>
-				<h3>Amanda Fernandes</h3>
-				<div className={styles.subTitle}>
-					<h2>UX/UI</h2>
-					<h4>Designer</h4>
+				<div className={styles.namePhoto}>
+					<motion.img
+						src='/imgs/amanda.svg'
+						alt="foto de perfil"
+						initial={{ opacity: 0, scale: 0.8 }}
+						animate={{ opacity: 1, scale: 1 }}
+						transition={{ duration: 10 }}
+					/>
+					<h1>
+						{showText}
+						{showText.length < fullText.length ? cursorState : ''}
+					</h1>
+				</div>
+				<div className={styles.subTitleContainer}>
+					<div className={styles.subTitle}>
+						<h2>UX/UI</h2>
+						<h3>Designer</h3>
+					</div>
+					<p>Apaixonada por transformar <span>problemas</span> em <span>soluções digitais</span> acessíveis e intuitivas.</p>
 				</div>
 			</div>
-			<p>Apaixonada por transformar <span></span>problemas em soluções digitais acessíveis e intuitivas.</p>
-			<p>Vem conhecer meu cantinho!</p>
-		</div>
+			<h4>Vem conhecer meu cantinho!</h4>
+			<img className={styles.arrowDown} src="/imgs/downarrow.svg" alt="flecha apontando para baixo" />
+		</motion.section>
 	)
 }
