16c5f1d7d1c7b04e2fa56f593ea75dd599e930d1
bruno <bruno.santossilveira001@gmail.com>
2025-03-29 15:18:23 -0300

    started last projects on page home

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -6,25 +6,39 @@ export default function Home() {
 	return (
 		<section className={styles.homeContainer}>
 			<div className={styles.mainContent}>
-				<div className={styles.whoAmI}>
-					<motion.h1
-						initial={{ opacity: 0, scale: 0.8, y: 80 }}
-						animate={{ opacity: 1, scale: 1, y: 0 }}
-						transition={{ duration: 2, delay: 0 }}
-					>Sou Amanda Passos,</motion.h1>
-					<h2>UX/UI Designer</h2>
+				<div className={styles.whoAmIContainer}>
+					<div className={styles.whoAmI}>
+						<motion.h1
+							initial={{ opacity: 0, scale: 0.8, y: 80 }}
+							animate={{ opacity: 1, scale: 1, y: 0 }}
+							transition={{ duration: 2, delay: 0 }}
+						>Sou Amanda Passos,</motion.h1>
+						<h2>UX/UI Designer</h2>
+					</div>
+					<h3>
+						Apaixonada por criar interfaces intuitivas, acessíveis e 
+						centradas no usuário. Meu objetivo é <span>transformar experiências </span> 
+						digitais em algo fluido, envolvente e funcional.
+					</h3>
+					<div className={styles.network}>
+						<a href="https://www.behance.net/amandagpf" target='_blank' title='Behance'> <AiFillBehanceCircle /> </a>
+						<a href=""> <AiFillLinkedin /> </a>
+						<a href=""> <AiFillGithub /> </a>
+					</div>
 				</div>
-				<h3>
-					Apaixonada por criar interfaces intuitivas, acessíveis e 
-					centradas no usuário. Meu objetivo é <span>transformar experiências </span> 
-					digitais em algo fluido, envolvente e funcional.
-				</h3>
-				<div className={styles.network}>
-					<AiFillBehanceCircle />
-					<AiFillLinkedin />
-					<AiFillGithub />
+
+				<div className={styles.lastProjectsContainer}>
+					<h1>Últimos projetos publicados:</h1>
+					<div className={styles.lastProjects}>
+						<div className={styles.projectCardIos}>
+							<h2>Habilita Já</h2>
+							
+						</div>
+						<div className={styles.projectCardAndroid}>
+
+						</div>
+					</div>
 				</div>
-				
 			</div>
 		</section>
 	)
